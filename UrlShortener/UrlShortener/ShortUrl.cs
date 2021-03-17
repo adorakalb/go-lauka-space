2c9959aadb938d2721370793ec54afecd3b5c01e
lauralani <dev@lauka.space>
2021-03-17 22:19:44 +0100

    add short url class and stuff

---
+++ UrlShortener/UrlShortener/ShortUrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortener
+{
+    class ShortUrl : Table
+    {
+        public string Extension { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
