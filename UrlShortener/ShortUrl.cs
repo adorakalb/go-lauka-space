54c23ac1d2177f330dc535a817c5ac0756174a56
lauralani <dev@lauka.space>
2021-03-18 08:29:41 +0100

    moved project content one folder up

--- UrlShortener/ShortUrl.cs
+++ UrlShortener/ShortUrl.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UrlShortener
 {
-    class ShortUrl : Table
+    class ShortUrl : TableEntity
     {
         public string Extension { get; set; }
         public string RedirectUrl { get; set; }
